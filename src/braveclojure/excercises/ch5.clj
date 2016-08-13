043c67fa504a59c534a95bd7fe9bc27151c1baca
Wen-Chun Lin <lovecankill@gmail.com>
2016-08-13 23:59:59 +0800

    Finish excercise of ch5

--- src/braveclojure/excercises/ch5.clj
+++ src/braveclojure/excercises/ch5.clj
@@ -19,3 +19,21 @@
           (rest fns))))))
 
 ;; 3. Implement the assoc-in function.
+(defn my-assoc-in
+  [m [k & ks] v]
+  (if ks
+    (assoc m k (my-assoc-in (get m k) ks v))
+    (assoc m k v)))
+
+;; 4. Look up and use the update-in function.
+(def users [{:name "James" :age 26}  {:name "John" :age 43}])
+(update-in users [1 :age] inc)
+
+(update-in {:a 3} [:a] / 4 5)
+
+;; 5. Implement update-in.
+(defn my-update-in
+  [m [k & ks] f & args]
+  (if ks
+    (assoc m k (my-update-in (get m k) ks f args))
+    (assoc m k (apply f (get m k) args))))
